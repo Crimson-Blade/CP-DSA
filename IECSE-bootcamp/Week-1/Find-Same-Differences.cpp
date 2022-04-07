8720b4a329ddfa5bc73141500258e25ff5667592
Rudeus <none>
2022-04-08 01:36:01 +0530

    Find same

--- IECSE-bootcamp/Week-1/Find-Same-Differences.cpp
+++ IECSE-bootcamp/Week-1/Find-Same-Differences.cpp
@@ -1,40 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int run(int a[], int &n)
-{
-    int count=0;
-    for(int i = 0;i < n;i++)
-    {
-        for(int j = i + 1;j < n;j++){
-            if((j-i) == (a[j]-a[i]))
-            count++;
-
-        }
-    }
-    return count;
-}
- 
-// Driver code
-int main()
-{
-    int t,tO;
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout<<"testcases";
+    int t;          // test cases
     cin>>t;
-    tO = t;
-    int answers[t];
     while(t>0){
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i = 0;i < n;i++)
-    {
-        cin >> a[i];
+
+    cout<<"size of arr";
+        int n; cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        for(int x:arr){
+            cout<<x<<"\n";
+        }
+
+        t--;
     }
-    // Print the count of unique pairs
-    answers[t-1] = ((run(a, n)));
-    t--;
-    }
-    for(int i = tO;i > 0;i--){
-    cout<<answers[i-1]<<"\n";
-    }
+
     return 0;
 }
