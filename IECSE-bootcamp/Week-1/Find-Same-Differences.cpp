497ca28af5ebea80399f951e9ca7952e8dfb259c
Rudeus <none>
2022-04-08 13:35:17 +0530

    Add Image

--- IECSE-bootcamp/Week-1/Find-Same-Differences.cpp
+++ IECSE-bootcamp/Week-1/Find-Same-Differences.cpp
@@ -3,21 +3,23 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
-    cout<<"testcases";
     int t;          // test cases
     cin>>t;
     while(t>0){
-
-    cout<<"size of arr";
+        int ans=0;
         int n; cin>>n;
         int arr[n];
+        map<int,int> diff;
+        map<int,int> counter;
         for(int i=0;i<n;i++){
             cin>>arr[i];
+            diff[i] = arr[i] - i;
+            counter[diff[i]]++;
         }
-        for(int x:arr){
-            cout<<x<<"\n";
+        for(auto x:counter){
+            ans+= (x.second * (x.second -  1))/2;
         }
-
+        cout<<ans<<"\n";
         t--;
     }
 
