da59739b2f203f3934439700929ba8f2686bc7d2
Rudeus <none>
2022-04-11 17:02:02 +0530

    Add P&C formulae

---
+++ IECSE-bootcamp/Week-1/We-Love-Odd.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool cmp(int a, int b){
+    return a>b;
+}
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    int t;          // test cases
+    cin>>t;
+    while(t>0){
+        //declaration
+        int n,counter=0;
+        cin>>n;
+        set<int,greater<int>> input;
+        set<int,greater<int>> :: iterator itr;
+
+        //Inputing Array (Only Even numbers)
+        for(int i=0;i<n;i++){
+            int temp;
+            cin>>temp;
+            if(temp%2==0){
+                input.insert(temp);
+            }
+        }
+        //Solution
+        while(input.size()>0){
+
+            itr = input.begin();
+            input.erase(*itr);
+            if(*itr%2==0 && (*itr/2)%2==0)
+            input.insert(*itr/2);
+            counter++;
+        }
+        cout<<counter<<"\n";
+        t--;
+    }
+    return 0;
+}
