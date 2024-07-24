d54585f3746747796e845d43bd7dc9fa38ff1a63
Lance <71289117+Crimson-Blade@users.noreply.github.com>
2024-07-24 20:12:14 +0530

    Time: 4190 ms (30.58%), Space: 31.8 MB (64.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,10 +3,9 @@ class Solution:
         ms = set(nums)
         output = 0
         for num in nums:
-            if num - 1 in ms:
-                continue
-            seq = 0
-            while (num + seq) in ms:
-                seq += 1
-            output = max(seq,output)
+            if num - 1 not in ms:
+                seq = 0
+                while (num + seq) in ms:
+                    seq += 1
+                output = max(seq,output)
         return output
