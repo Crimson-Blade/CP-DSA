e616a8115085a0de913a605140668d0944274d25
Lance <71289117+Crimson-Blade@users.noreply.github.com>
2024-10-13 14:11:49 +0530

    Time: 40 ms (58.23%), Space: 16.7 MB (40.15%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # Base Case
+        res = []
+        if len(nums) == 0:
+            return [[]]
+        perms = self.permute(nums[1:])
+        for p in perms:
+            for i in range(len(p)+1):
+                p_copy = p.copy()
+                p_copy.insert(i,nums[0])
+                res.append(p_copy)
+        return res
+                
+        
